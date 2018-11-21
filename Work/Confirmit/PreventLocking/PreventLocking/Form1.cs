f284d3f39d22cf84aa249898553b38b685796f97
FIRM\grigoryk <grigory.konovalov@confirmit.com>
2018-11-21 11:08:07 +0100

    Make PreventLocking app

---
+++ Work/Confirmit/PreventLocking/PreventLocking/Form1.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace PreventLocking
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+
+        [Flags]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            
+            SetThreadExecutionState(EXECUTION_STATE.ES_DISPLAY_REQUIRED | EXECUTION_STATE.ES_CONTINUOUS);
+
+            try
+            {
+                AddToAutoStart();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"App cannot be added to autoraun because an error: {ex.Message}. App continue to work as usual.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void buttonExcludeFromAutostart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                RemoveFromAutostart();
+
+                MessageBox.Show("App was removed from autorun. It will be added to autorun after next start.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("App cannot be removed from autoraun because an error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        const string AutostartRegPath = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
+        private void AddToAutoStart()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(AutostartRegPath, true))
+            {
+                key.SetValue("PreventLockingApp", Application.ExecutablePath);
+            }
+        }
+
+        private void RemoveFromAutostart()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(AutostartRegPath, true))
+            {
+                key.DeleteValue("PreventLockingApp", false);
+            }
+        }
+    }
+}
